18018f1ef253b1940dadf68dc230f62297972946
James Savage <axiixc@gmail.com>
2011-10-19 02:11:27 -0400

    Fixed missing .nil? check in profile page

--- routes/profile.rb
+++ routes/profile.rb
@@ -1,5 +1,5 @@
 get '/profile/:token' do |token|
-   @profile_user = @session_user.username == token ? @session_user : Rose::User.first(:username => token, :public_stats => true)
+   @profile_user = (!@session_user.nil? && @session_user.username == token) ? @session_user : Rose::User.first(:username => token, :public_stats => true)
    @profile_user = Rose::User.first(:protected_stats_token => token) if @profile_user.nil?
    if @profile_user.nil?
       return "Invalid user profile"
